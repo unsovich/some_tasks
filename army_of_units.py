ef0fd48e76e819d84a0f4f9d9e7920ec2fbf9afa
DmitiryKalekin <herrhorror@gmail.com>
2023-05-29 09:51:01 +0600

    solution

--- army_of_units.py
+++ army_of_units.py
@@ -21,47 +21,61 @@ def perform_damage()
 
 class Unit:
     def __init__(self, damage):  # атрибут урон
-        self.damage = damage
+        self._damage = damage
 
     def make_damage(self):  # сила урона
-        return self.damage
+        return self._damage
 
 
-class Army:
+class Archer(Unit):
     def __init__(self):
-        self.units = []  # список юнитов
+        super().__init__(damage=1)
 
-    def add_unit(self, unit):
-        self.units.append(unit)  # добавление юнитов в армию
+
+class Cannon(Unit):
+    def __init__(self):
+        super().__init__(damage=5)
+
+
+class Army(Unit):
+    def __init__(self):
+        self._units = []  # список юнитов
+        super().__init__(damage=0)
+
+    def add_unit(self, unit: Unit):
+        self._units.append(unit)  # добавление юнитов в армию
+
+    def remove_unit(self, unit: Unit):
+        self._units = [u for u in self._units if u != unit]
+
+    def print_units(self):
+        for u in self._units:
+            print(f"{u.__class__.__name__} makes {u.make_damage()} damage")
 
     def make_damage(self):
-        total_damage = sum(unit.make_damage() for unit in self.units)  # суммирует урон юнита
+        total_damage = sum(unit.make_damage() for unit in self._units)  # суммирует урон юнита
         return total_damage
 
-
-archer = Unit(1)
-cannon = Unit(5)
-
-army1 = Army()
-army1.add_unit(archer)
-army1.add_unit(cannon)
-
-army2 = Army()
-army2.add_unit(archer)
-army2.add_unit(cannon)
-
-army3 = Army()
-army3.add_unit(archer)
-army3.add_unit(cannon)
+if __name__ == "__main__":
+    archer = Archer()
+    cannon = Cannon()
+    other = Unit(damage=100)
 
 
-def maximum_damage(armies):  # суммарный урон от армии
-    max_dmg = 0
-    for army in armies:
-        max_dmg += army.make_damage()
-    return max_dmg
+    army1 = Army()
+    army1.add_unit(archer)
+    army1.add_unit(cannon)
+    army1.add_unit(other)
+    print("===============")
+    print(f"Army1 damage={army1.make_damage()}")
+    army1.print_units()
+
+    army2 = Army()
+    army2.add_unit(archer)
+    army2.add_unit(cannon)
+    army2.add_unit(army1)
+    print("===============")
+    print(f"Army2 damage={army2.make_damage()}")
+    army2.print_units()
 
 
-armies = [army1, army2, army3]
-max_damage = maximum_damage(armies)
-print("Максимальный урон от армий:", max_damage)
