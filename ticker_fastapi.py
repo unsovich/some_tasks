3ccbeb4fcf4184d9bdba4e0925a63221b0cd7b17
unsovich <unsovich@gmail.com>
2023-05-31 12:40:09 -0300

    Added task ticker using FastAPI

---
+++ ticker_fastapi.py
@@ -0,0 +1,22 @@
+import asyncio
+from fastapi import FastAPI
+import datetime
+
+app = FastAPI()
+
+
+async def ticker():
+    while True:
+        print("Tick", datetime.datetime.now().strftime("%H:%M:%S"))
+        await asyncio.sleep(1)
+
+
+@app.on_event("startup")
+async def startup_event():
+    asyncio.create_task(ticker())
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
